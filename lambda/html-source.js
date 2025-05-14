1737b7c957b3edabad7eb3571026124d41dc5369
Rohan Chopra <rohanchopra@outlook.com>
2025-05-14 10:02:58 +0100

    Adds fetch headers

--- lambda/html-source.js
+++ lambda/html-source.js
@@ -3,13 +3,21 @@
 // set timeout to 32 secs
 
 export const handler = async(event) => {
-    let url    = event.url || event.queryStringParameters?.url;
-    let text   = '';
-    let status = 500; // assume the worst
+    let url     = event.url || event.queryStringParameters?.url;
+    let text    = '';
+    let status  = 500; // assume the worst
+    let base    = new URL(url).origin;
+    let method  = 'GET';
+    let headers = {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120.0 Safari/537.36',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+      'Referer': base + '/',
+      'Origin': base
+    };
 
     try
     {
-        let response = await fetch(url);
+        let response = await fetch(url, { method, headers });
         status = response.status;
         text   = await response.text();
     }
